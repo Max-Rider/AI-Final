c1d1eeae0062d1e59753d9ee7b0a905a2ba96542
Max Rider <ridermax@comcast.net>
2020-12-11 22:02:32 -0500

    implemented getMapProperties and getLegalMoves functions

--- enemy.py
+++ enemy.py
@@ -1,43 +1,95 @@
 """
 File for enemy functions (AI functionality for movement and attacks and things)
 """
+import pygame
+import game
+from pygame.locals import *
+#from rewardArrs import Rewards
 from pyqlearning.qlearning.greedy_q_learning import GreedyQLearning
-import rewardarrs
-
+from pytmx.util_pygame import load_pygame
     
     
 class Enemy():
     def __init__(self):
         pass
     
-    def getLegalActions(self, currState):
+    def getMapProperties(self, x, y):
+        xTile = x // 16
+        yTile = y // 16
+        
+        
+        wallProp = game.Game().tmxdata.get_tile_properties(xTile, yTile, 0)
+        #print("got wall props")
+        if wallProp is None:
+            wallProp = {"solid":0, "stairs":0}
+        return wallProp
+    
+    def getLegalActions(self, x, y):
         """
         Get a list of the legal actions from the current state
         
         *use a similar method used for stopping the player 
-            movement to find find the legal moves
-                (if the adjacent tile is solid then it is not legal)
-        return a list of North, South, East, West 
+            movement to find find the legal moves (look at getMapProperties and move)
+                (i.e. if the adjacent tile is solid then it is not legal)
+                    (this also means we don't have to worry 
+                    about the reward value of a wall location)
+                    
+        returns a list of North, South, East, or West 
                 which will correspond to movement vectors 
                 (i.e. North = (0,-16) it's -16 because pygame 
                 has the origin at the top left and the blocks are each 16 pixels long/wide)
-                
+        """
+        
+        legalActions = []
+        
+        westTile = self.getMapProperties(x-5, y)
+        eastTile = self.getMapProperties(x+64, y)
+        northTile = self.getMapProperties(x, y)
+        southTile = self.getMapProperties(x, y+64)
+        
+        if westTile[0]['solid'] == 1:
+            legalActions = ["North", "South", "East"]
+        elif eastTile[0]['solid'] == 1:
+            legalActions = ["North", "South", "West"]
+        elif northTile[0]['solid'] == 1:
+            legalActions = ["South", "East", "West"]
+        elif southTile[0]['solid'] == 1:
+            legalActions = ["North", "East", "West"]
+        else:
+            legalActions = ["North", "South", "East", "West"]
+            
+        return legalActions
+    
+    def getRewardVal(self, state, world):
+        """
+        given a state, get the reward value for that state
         """
         pass
     
-    def moveAgent(self):
+    def moveAgent(self, x, y, tmxdata):
         """
+        Similar to 'update' function in qlearningAgents.py from hw
         use pyqlearning functions to compute Q(s,a)
         funcs available to use:
             *get_epsilon_greedy_rate(self)
             *set_epsilon_greedy_rate(self, value)
             *select_action(self, state_key, next_action_list)
             
-        need to define some other funcs though:
-            *getLegalActions()
-            *getRewardVal
-            *initialize => this func would prepare everything like
+            use Q(s,a) to move the agent
+            
+        need to define some other funcs first though:
+            *getLegalActions() => return legal actions from curState
+            *getRewardVal() => get the reward value of a given state
+            *initialize() => this func would prepare everything like
                             the map data and the reward data and things like that
             *
         """
+        
+        legalActions = self.getLegalActions(x, y)
+        
+        GreedyQLearning.select_action((x, y), legalActions)
+        
+        # returns a tuple of coordinates that the main game loop uses to update the position
+        return (x, y)
+        
         pass
--- rewardArrs.py
+++ rewardArrs.py
@@ -17,10 +17,29 @@ class Rewards:
                    [-100,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-100],
                    [-100,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-100],
                    [-100,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-100],
-                   [-100,0,0,0,0,0,0,0,0,0,0,,85,90,95,90,85,80,75,-100],
+                   [-100,0,0,0,0,0,0,0,0,0,0,80,85,90,95,90,85,80,75,-100],
                    [-100,35,40,45,50,55,60,65,70,75,80,85,90,95,100,95,90,85,80,-100],
                    [-100,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-100],
                    [-100,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-100],
                    [-100,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-100],
                    [-100,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-100],
                    [-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100]]
+    
+    
+    
+    def trainer0():
+        """
+        function to automate the process of creating a 20x20 array of 
+        reward values that converge on the player position 
+            (player position being the highest reward 
+            with the rewards shrinking the father from the player you get)
+            
+        The maximum value (i.e. the value at the player pos) would be the maximum 
+        distance from any corner of the grid. Say the player has coordinates x,y. 
+        The corners are at 0,0 19,0 0,19 and 19,19. Then the maximum value 
+        would be the maximum of x+y, 19-x+y, x+19-y and 19-x+19-y. 
+        Then, for each point in the grid with coordinates i,j you enter 
+        (max value - distance from x,y to i,j), 
+        so the neighbors of * have maxvalue -1, their neighbors have maxvalue -2, etc. 
+        
+        """
